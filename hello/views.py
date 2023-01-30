e2504b98d99694adc73cf23c95fdc4dec0ce9ea5
sumiok <tap.miwo@gmail.com>
2023-01-31 05:14:29 +0900

    use params

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return render(request, 'hello/index.html')
+  params = {
+    'title': 'Hello/Index',
+    'msg': 'これはサンプルで作ったページです'
+  }
+  return render(request, 'hello/index.html', params)
