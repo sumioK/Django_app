df2d2cd4b59e45cf95bb4223b291c73cb24593b6
sumiok <tap.miwo@gmail.com>
2023-01-30 21:16:43 +0900

    templates

--- hello/urls.py
+++ hello/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('my_name_is_<nickname>.I_am_<int:age>_years_old.',views.index, name='index'),
+  path('', views.index, name='index'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request, nickname, age):
-  result = 'your account: ' + nickname + ' (' + str(age) + ').'
-  return HttpResponse(result)
+def index(request):
+  return render(request, 'hello/index.html')
