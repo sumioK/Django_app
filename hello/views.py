5ff83669236af2fab3b9a414f65b15cc67e949e8
sumiok <tap.miwo@gmail.com>
2023-01-31 05:57:17 +0900

    next page some templates

--- hello/views.py
+++ hello/views.py
@@ -4,6 +4,15 @@ from django.http import HttpResponse
 def index(request):
   params = {
     'title': 'Hello/Index',
-    'msg': 'これはサンプルで作ったページです'
+    'msg': 'これはサンプルで作ったページです',
+    'goto': 'next',
+  }
+  return render(request, 'hello/index.html', params)
+
+def next(request):
+  params = {
+    'title': 'Hello/Next',
+    'msg': 'これはもう一つのページです',
+    'goto': 'index',
   }
   return render(request, 'hello/index.html', params)
