42c0e1fd004e9eafd388204fc94247f9dfd785b6
sumiok <tap.miwo@gmail.com>
2023-02-01 20:05:28 +0900

    FormClass bootstrap

--- hello/forms.py
+++ hello/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class HelloForm(forms.Form):
-  name = forms.CharField(label='name')
-  mail = forms.CharField(label='mail')
-  age = forms.IntegerField(label='age')
+  name = forms.CharField(label='name', widget=forms.TextInput(attrs={'class':'form-control'}))
+  mail = forms.CharField(label='mail', widget=forms.TextInput(attrs={'class':'form-control'}))
+  age = forms.IntegerField(label='age', widget=forms.NumberInput(attrs={'class':'form-control'}))
