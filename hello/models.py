01a9f51795f8be10c6fb872f866755c55fe6ea89
sumiok <tap.miwo@gmail.com>
2023-02-02 09:08:09 +0900

    models Friend

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Friend(models.Model):
+  name = models.CharField(max_length=100)
+  mail = models.EmailField(max_length=200)
+  gender = models.BooleanField()
+  age = models.IntegerField(default=0)
+  birthday = models.DateField()
+
+  def __str__(self):
+    return '<Friend:id={},{}({})>'.format(str(self.id), \
+            self.name, str(self.age))
