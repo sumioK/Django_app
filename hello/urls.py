279d553bb539b1de65569e1c70d7f9d747fbc6b3
sumiok <tap.miwo@gmail.com>
2023-02-02 09:56:10 +0900

    get data from models

--- hello/urls.py
+++ hello/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import HelloView
+from . import views
 
 urlpatterns = [
-  path(r'', HelloView.as_view(), name='index'),
+  path('', views.index, name='index')
 ]
