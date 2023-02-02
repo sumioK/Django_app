410e95390a66bc57f8e7e8c41be918023bbe7a39
sumiok <tap.miwo@gmail.com>
2023-02-02 21:20:06 +0900

    meddle of crud

--- hello/forms.py
+++ hello/forms.py
@@ -1,4 +1,8 @@
 from django import forms
 
 class HelloForm(forms.Form):
-  id = forms.IntegerField(label='ID')
+  name = forms.CharField(label='Name', widget=forms.TextInput(attrs={'class':'form-control'}))
+  mail = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={'class':'form-control'}))
+  gender = forms.BooleanField(label='Gender', required=False, widget=forms.CheckboxInput(attrs={'class':'form-check'}))
+  age = forms.IntegerField(label='Age', widget=forms.NumberInput(attrs={'class':'form-control'}))
+  birthday = forms.DateField(label='Birth', widget=forms.DateInput(attrs={'class': 'form-control'}))
