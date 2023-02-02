8b8af41d9dc0d704cc06a9870f0350ce7d85214c
sumiok <tap.miwo@gmail.com>
2023-02-02 22:41:16 +0900

    create data

--- hello/views.py
+++ hello/views.py
@@ -1,39 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Friend
-from django.db.models import QuerySet
-
-# def index(request):
-#   params = {
-#     'title': 'Hello',
-#     'message': 'all friends.',
-#     'form': HelloForm,
-#     'data': [],
-#   }
-#   if(request.method == 'POST'):
-#     num=request.POST['id']
-#     item = Friend.objects.get(id=num)
-#     params['data'] = [item]
-#     params['form'] = HelloForm(request.POST)
-#   else:
-#     params['data'] = Friend.objects.all()
-#   return render(request, 'hello/index.html', params)
-
-def __new_str__(self):
-  result = ''
-  for item in self:
-    result += '<tr>'
-    for k in item:
-      result += '<td>' +str(k) + '=' + str(item[k]) + '</td>'
-    result += '</tr>'
-  return result
-
-QuerySet.__str__ = __new_str__
+from .forms import HelloForm
 
 def index(request):
-  data = Friend.objects.all().values('id', 'name', 'age')
+  data = Friend.objects.all()
   params = {
     'title': 'Hello',
     'data': data,
   }
   return render(request, 'hello/index.html', params)
+
+# create model
+def create(request):
+  params = {
+    'title': 'Hello',
+    'form': HelloForm(),
+  }
+  if (request.method == 'POST'):
+    name = request.POST['name']
+    mail = request.POST['mail']
+    gender = 'gender' in request.POST
+    age = int(request.POST['age'])
+    birth = request.POST['birthday']
+    friend = Friend(name=name, mail=mail, gender=gender, age=age, birthday=birth)
+    friend.save()
+    return redirect(to='/hello')
+  return render(request, 'hello/create.html', params)
