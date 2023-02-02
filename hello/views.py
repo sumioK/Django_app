bbdd6a353601d36d577f824cc8d7175361549f98
sumiok <tap.miwo@gmail.com>
2023-02-02 13:05:27 +0900

    all_values_list

--- hello/views.py
+++ hello/views.py
@@ -3,18 +3,25 @@ from django.http import HttpResponse
 from .models import Friend
 from .forms import HelloForm
 
+# def index(request):
+#   params = {
+#     'title': 'Hello',
+#     'message': 'all friends.',
+#     'form': HelloForm,
+#     'data': [],
+#   }
+#   if(request.method == 'POST'):
+#     num=request.POST['id']
+#     item = Friend.objects.get(id=num)
+#     params['data'] = [item]
+#     params['form'] = HelloForm(request.POST)
+#   else:
+#     params['data'] = Friend.objects.all()
+#   return render(request, 'hello/index.html', params)
 def index(request):
+  data = Friend.objects.all().values_list('id', 'name', 'age')
   params = {
     'title': 'Hello',
-    'message': 'all friends.',
-    'form': HelloForm,
-    'data': [],
+    'data': data
   }
-  if(request.method == 'POST'):
-    num=request.POST['id']
-    item = Friend.objects.get(id=num)
-    params['data'] = [item]
-    params['form'] = HelloForm(request.POST)
-  else:
-    params['data'] = Friend.objects.all()
   return render(request, 'hello/index.html', params)
