6227afe1c5ea83c7a9816a3aba224d6a0f021054
sumiok <tap.miwo@gmail.com>
2023-02-03 14:41:16 +0900

    edit

--- hello/views.py
+++ hello/views.py
@@ -23,3 +23,16 @@ def create(request):
     'form': FriendForm(),
   }
   return render(request, 'hello/create.html', params)
+
+def edit(request, num):
+  obj = Friend.objects.get(id=num)
+  if request.method == 'POST':
+    friend = FriendForm(request.POST, instance=obj)
+    friend.save()
+    return redirect(to='/hello')
+  params = {
+    'title': 'Hello',
+    'id': num,
+    'form': FriendForm(instance=obj),
+  }
+  return render(request, 'hello/edit.html', params)
