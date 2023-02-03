13916e42078474e01bc9dab786a72bfe2ec2ba13
sumiok <tap.miwo@gmail.com>
2023-02-03 14:10:14 +0900

    use ModelForm

--- hello/forms.py
+++ hello/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from.models import Friend
 
 class HelloForm(forms.Form):
   name = forms.CharField(label='Name', widget=forms.TextInput(attrs={'class':'form-control'}))
@@ -6,3 +7,8 @@ class HelloForm(forms.Form):
   gender = forms.BooleanField(label='Gender', required=False, widget=forms.CheckboxInput(attrs={'class':'form-check'}))
   age = forms.IntegerField(label='Age', widget=forms.NumberInput(attrs={'class':'form-control'}))
   birthday = forms.DateField(label='Birth', widget=forms.DateInput(attrs={'class': 'form-control'}))
+
+class FriendForm(forms.ModelForm):
+  class Meta:
+    model = Friend
+    fields = ['name', 'mail', 'gender', 'age', 'birthday']
--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Friend
-from .forms import HelloForm
+from .forms import FriendForm
 
 def index(request):
   data = Friend.objects.all()
@@ -13,17 +13,13 @@ def index(request):
 
 # create model
 def create(request):
-  params = {
-    'title': 'Hello',
-    'form': HelloForm(),
-  }
-  if (request.method == 'POST'):
-    name = request.POST['name']
-    mail = request.POST['mail']
-    gender = 'gender' in request.POST
-    age = int(request.POST['age'])
-    birth = request.POST['birthday']
-    friend = Friend(name=name, mail=mail, gender=gender, age=age, birthday=birth)
+  if request.method == 'POST':
+    obj = Friend()
+    friend = FriendForm(request.POST, instance=obj)
     friend.save()
     return redirect(to='/hello')
+  params = {
+    'title': 'Hello',
+    'form': FriendForm(),
+  }
   return render(request, 'hello/create.html', params)
